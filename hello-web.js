5eae04ef2f66406e5f66bac9675f11906d8229ee
Shilpi Sinha <shilpi.sinha@stonetusker.com>
2025-11-27 08:23:31 +0530

    Add hello-web.js file

---
+++ hello-web.js
@@ -0,0 +1,17 @@
+// hello-web.js
+
+const http = require("http");
+
+const PORT = 4000;
+const HOST = "0.0.0.0"; // listen on all network interfaces
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;                        // HTTP status: 200 OK
+  res.setHeader("Content-Type", "text/html");  // Tell browser it's HTML
+  res.end("Hello World.... from Stonetusker!!!!\n");
+});
+
+server.listen(PORT, HOST, () => {
+  console.log(`Server running at http://${HOST}:${PORT}/`);
+});
+
