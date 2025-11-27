b274dda1c15e8f259d24e27d77699899ee347afb
Shilpi Sinha <shilpi.sinha@stonetusker.com>
2025-11-27 11:29:09 +0530

    Change hello message

--- hello-web.js
+++ hello-web.js
@@ -8,7 +8,7 @@ const HOST = "0.0.0.0"; // listen on all network interfaces
 const server = http.createServer((req, res) => {
   res.statusCode = 200;                        // HTTP status: 200 OK
   res.setHeader("Content-Type", "text/html");  // Tell browser it's HTML
-  res.end("Hello World.... from Stonetusker!!!!\n");
+  res.end("Hello World.... from Stonetusker — where ideas take shape.!!!!\n");
 });
 
 server.listen(PORT, HOST, () => {
